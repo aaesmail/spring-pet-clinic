eeab7f27fc737d11fdb4eda06c24c01f5d3837bc
Ali Adel <aliadel56000@gmail.com>
2022-02-17 12:30:46 +0200

    Refactor to use project lombok

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Person.java
@@ -3,6 +3,15 @@ package com.example.petclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 
@@ -12,19 +21,10 @@ public class Person extends BaseEntity {
     @Column(name = "last_name")
     private String lastName;
 
-    public String getFirstName() {
-        return this.firstName;
-    }
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
 
-    public void setFirstName(String firstName) {
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
